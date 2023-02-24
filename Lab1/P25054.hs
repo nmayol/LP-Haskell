f49397bc18b4527a0104ec6b4d45402f3b11f606
nmayol <02mayolneus@gmail.com>
2023-02-24 20:37:49 +0100

    no funciona

--- Lab1/P25054.hs
+++ Lab1/P25054.hs
@@ -19,7 +19,6 @@ average list = s / m
         m = fromIntegral (myLength list) :: Float
 
 
-
 buildPalindrome :: [Int] -> [Int]
 buildPalindrome list = reverse list ++ list
 
@@ -31,4 +30,45 @@ remove (x:xs) y
 
 flatten :: [[Int]] -> [Int] 
 flatten [] = []
-flatten _ =  
+flatten (x:xs) = x ++ flatten xs 
+
+evens :: [Int] -> [Int]
+evens [] = []
+evens (x:xs)
+    | x `mod` 2 == 0 = (x:(evens xs))
+    | otherwise = evens xs
+
+odds :: [Int] -> [Int]
+odds [] = []
+odds (x:xs)
+    | x `mod` 2 /= 0 = (x:(odds xs))
+    | otherwise = odds xs
+
+oddsNevens :: [Int] -> ([Int],[Int])
+oddsNevens list = (odds list, evens list)
+
+-- void primeFactors(int n)
+-- {
+--   int c = 2;
+--   while (n > 1) {
+--     if (n % c == 0) {
+--       printf("%d ", c);
+--       n /= c;
+--     }
+--     else
+--       c++;
+--   }
+-- }
+
+primes :: Int -> Int -> [Int]
+primes 1 _ = []
+primes x y
+    | x `mod` y == 0 && elem(y (primes ((x `div` y) (y)))) = primes ((x `div` y) (y))
+    | x `mod` y == 0 = (y:primes (x `div` y) (y))
+    | y == floor (sqrt (fromIntegral x)) = x:[]
+    | otherwise = primes x (y+1)
+
+
+primeDivisors :: Int -> [Int]
+primeDivisors 1 = []
+primeDivisors n = primes n 2
